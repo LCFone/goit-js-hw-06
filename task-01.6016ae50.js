74c1bf359f6e992ac1b297957307edc9f3d825d1
LCFone <133873241+LCFone@users.noreply.github.com>
2023-08-25 18:29:40 +0000

    Deploying to gh-pages from @ LCFone/goit-js-hw-06@b105e705d1b609ace6e86d704e36a1df72bbd550 🚀

---
+++ task-01.6016ae50.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelectorAll("#categories .item");console.log("Number of categories: ".concat(e.length)),e.forEach((function(e){var o=e.querySelector("h2").textContent,t=e.querySelectorAll("ul li").length;console.log("Category: ".concat(o)),console.log("Elements: ".concat(t))}))}();
+//# sourceMappingURL=task-01.6016ae50.js.map
