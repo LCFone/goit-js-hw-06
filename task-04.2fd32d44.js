6f5040f9f755147eed9ceef1479c13d3a91735d6
LCFone <133873241+LCFone@users.noreply.github.com>
2023-08-26 11:10:28 +0000

    Deploying to gh-pages from @ LCFone/goit-js-hw-06@787ce4c8f6a26a33aeafb7604eb190e6e0b1f11b 🚀

--- task-04.2fd32d44.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("#counter"),t=(document.querySelector("#value"),e.querySelector('[data-action="increment"]')),n=e.querySelector('[data-action="decrement"]'),c=0;function o(){valueSpan.textContent=c}t.addEventListener("click",(function(){c+=1,o()})),n.addEventListener("clickk",(function(){c-=1,o()})),o();
-//# sourceMappingURL=task-04.2fd32d44.js.map
--- task-04.b6675cf6.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("#counter"),t=(document.querySelector("#value"),e.querySelector('[data-action="increment"]')),n=e.querySelector('[data-action="decrement"]'),c=0;function o(){valueSpan.textContent=c}t.addEventListener("click",(function(){c+=1,o()})),n.addEventListener("clickk",(function(){c-=1,o()})),o()}();
-//# sourceMappingURL=task-04.b6675cf6.js.map
