d1b45aa5e02aa1419b267adf12d87d10027cf1e3
LCFone <putilov.k@gmail.com>
2023-08-26 12:49:10 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,3 +1,6 @@
+// поблю посилання на список для ul#ingridients
+const ingredientsList = document.querySelector('#ingredients');
+
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +9,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// цикл для сворення та додавання елементу li до списку
+ingredients.forEach((ingredient) => {
+  // створюю новий елемент ls
+const listItem = document.createElement('li');
+
+// додаю нащву інгредієнта до li
+listItem.textContent = ingredient;
+
+// додаю клас item до елементу li
+listItem.classList.add('item');
+
+// додаю створенний lis до списку ul#ingredients
+ingredientsList.appendChild(listItem);
+
+});
