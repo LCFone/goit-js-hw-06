787ce4c8f6a26a33aeafb7604eb190e6e0b1f11b
LCFone <putilov.k@gmail.com>
2023-08-26 14:09:58 +0300

    Update task-04.js

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,32 +1,32 @@
 // отримаю посилання до елементів ДОМ
-const counter = document.querySelector('#counter');
-const value = document.querySelector('#value');
+const counter = document.getElementById('counter');
+const valueSpan = document.getElementById('value');
 const incrementButton = counter.querySelector('[data-action="increment"]');
 const decrementButton = counter.querySelector('[data-action="decrement"]');
 
 // ініціалізую змінну для зберігання поточного значення лічильника
-const counterValue = 0;
+let counterValue = 0;
 
 // функція оновлення тексту в span з поточним значенням
-function updateCounterTxt() {
-    valueSpan.textContent = counterValue; 
-}
+function updateCounterText() {
+    valueSpan.textContent = counterValue;
+  }
 
 // додаю  кліки до кнопки +
 incrementButton.addEventListener('click', function () {
     counterValue += 1; 
     // оновлюю текст span
-    updateCounterTxt();
+    updateCounterText();
 });
 
 // додаю  кліки до кнопки -
-decrementButton.addEventListener('clickk', function () {
+decrementButton.addEventListener('click', function () {
     counterValue -= 1;
-    updateCounterTxt();
+    updateCounterText();
 });
 
 // ініціалізую початковий текст span
-updateCounterTxt();
+updateCounterText();
 
 
 
