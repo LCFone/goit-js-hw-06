347169d830139a25598602f2cfacd10d00a376d6
LCFone <putilov.k@gmail.com>
2023-08-26 13:25:13 +0300

    Update task-03.js

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -1,3 +1,4 @@
+// масив оёэктыв зображень та альт текстом
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -12,3 +13,19 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+ 
+// роблю посилання на список ul.gallery
+const galleryList = document.querySelector(`.gallery`);
+
+// розмітка для галереї з шаблонних рядкі
+const galleryItems = images.map((image) => {
+  return `<li>
+    <img src="${image.url}" alt="${image.alt}">
+  </li>`;
+
+// об'єднаю всі рядки в один
+}).join('');
+
+// Вставляю нову розмітку в список ul.gallery
+galleryList.insertAdjacentHTML('beforeend', galleryItems);
+
