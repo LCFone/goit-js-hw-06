c4f2944f3d2c25657869297c6b5425d1afc1c27d
LCFone <133873241+LCFone@users.noreply.github.com>
2023-08-27 08:01:39 +0000

    Deploying to gh-pages from @ LCFone/goit-js-hw-06@284adc873909a7facc23d1e556362ee422cff7ee 🚀

---
+++ task-07.05316918.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("#fontSizeControl"),t=document.querySelector("#text");t.style.fontSize="16px",e.addEventListener("input",(function(){var e=this.value+"px";t.style.fontSize=e}))}();
+//# sourceMappingURL=task-07.05316918.js.map
